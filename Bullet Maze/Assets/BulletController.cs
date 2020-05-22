bba7e85ed7bff8518e06192f3b6967f92a5aea71
perryliuofficial <59033672+perryliuofficial@users.noreply.github.com>
2020-05-22 18:00:29 +0100

    Initial Commit

---
+++ Bullet Maze/Assets/BulletController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+
+    public float bulletSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Movement
+        Rigidbody ourRigidBody = GetComponent<Rigidbody>();
+        ourRigidBody.velocity = transform.forward * bulletSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision thiscollision) {
+        GameObject theirGameObject = thiscollision.gameObject;
+        if (theirGameObject.GetComponent<EnemyController>() != null) {
+            Destroy(theirGameObject);
+            Destroy(gameObject);
+        };
+    }
+}
---
+++ Bullet Maze/Assets/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    public GameObject bulletprefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Movement
+
+        Vector3 inputVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        Rigidbody ourRigidBody = GetComponent<Rigidbody>();
+        ourRigidBody.velocity = inputVector * speed;
+
+        //Find new position
+        // Vector3 newPosition = transform.position + inputVector;
+
+        // transform.position = newPosition; //Move to new position
+
+        //Face mouse
+        // Generate a plane that intersects the transform's position with an upwards normal.
+    	Plane playerPlane = new Plane(Vector3.up, transform.position);
+ 
+    	// Generate a ray from the cursor position
+    	Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+ 
+    	// Determine the point where the cursor ray intersects the plane.
+    	// This will be the point that the object must look towards to be looking at the mouse.
+    	// Raycasting to a Plane object only gives us a distance, so we'll have to take the distance,
+    	//   then find the point along that ray that meets that distance.  This will be the point
+    	//   to look at.
+    	float hitdist = 0.0f;
+    	// If the ray is parallel to the plane, Raycast will return false.
+    	if (playerPlane.Raycast (ray, out hitdist)) 
+		{
+        	// Get the point along the ray that hits the calculated distance.
+        	Vector3 targetPoint = ray.GetPoint(hitdist);
+ 
+        	// Determine the target rotation.  This is the rotation if the transform looks at the target point.
+        	Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+            transform.rotation = targetRotation;
+        	// Smoothly rotate towards the target point.
+        	//transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, speed * Time.deltaTime);
+		}
+
+        //Fire Bullet
+        if (Input.GetButton("Fire1")){
+            Instantiate(bulletprefab, transform.position + transform.forward, transform.rotation);
+        }
+    }
+}
