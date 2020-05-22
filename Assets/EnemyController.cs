a089fd51167678a217950323d298e809b593e5b2
perryliuofficial <59033672+perryliuofficial@users.noreply.github.com>
2020-05-22 22:38:28 +0100

    New AI Pathfinding

---
+++ Assets/EnemyController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+
+    public GameObject player;
+    public NavMeshAgent agent;
+    //public float enemySpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(player.transform.position);
+        // Rigidbody ourRigidBody = GetComponent<Rigidbody>();
+        // Vector3 vectorToPlayer = player.transform.position - transform.position;
+        // ourRigidBody.velocity = vectorToPlayer.normalized * enemySpeed;
+
+    }
+}
